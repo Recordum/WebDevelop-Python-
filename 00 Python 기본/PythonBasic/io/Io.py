3577a427548c5bfaa77224d4f854475429182467
mingyu <mingyu.oh15b@gmail.com>
2023-01-09 03:38:56 +0900

    기본 입출력

---
+++ 00 Python 기본/PythonBasic/io/Io.py
@@ -0,0 +1,14 @@
+# 기본 입출력
+import sys
+
+a = list(map(int, sys.stdin.readline().rstrip()))
+print(a)
+
+b = int(sys.stdin.readline().rstrip())
+print(b)
+
+c = list(map(int, input().split()))
+print(c)
+
+n,m,k = map(int,input().split())
+print(n,m,k)
