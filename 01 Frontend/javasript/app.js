14342ab4c5df5161c7bb55bd3b8513fc2ff6118c
mingyu <mingyu.oh15b@gmail.com>
2023-01-10 20:22:13 +0900

    javascript Event with CSS

--- 01 Frontend/javasript/app.js
+++ 01 Frontend/javasript/app.js
@@ -1,47 +1,13 @@
-const queryID = document.querySelector("#qID")
-const queryClass = document.querySelector(".qClass")
-const queryAll = document.querySelectorAll(".qClass")
-
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    queryID.style.color = "blue";
+    //.className : class 명을 getter/setter
+    const clickedClass = "active"
+//    if(h1.classList.contains(clickedClass)){
+//        h1.classList.remove(clickedClass);
+//    }else{
+//       h1.classList.add(clickedClass);
+    h1.classList.toggle(clickedClass);
 }
 
-function handleMouseEnter() {
-    queryClass.style.color = "blue";
-}
-
-function handleMouseLeave() {
-    queryClass.innerText = "Mouse was visited"
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor="tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("No WIFI");
-}
-
-
-function handleWindowOnline() {
-    alert("ALL GOOD")
-}
-
-queryID.style.color = "red";
-queryID.addEventListener("click", handleTitleClick);
-queryClass.addEventListener("mouseenter", handleMouseEnter);
-queryClass.addEventListener("mouseleave", handleMouseLeave);
-
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
-
+h1.addEventListener("click", handleTitleClick);
