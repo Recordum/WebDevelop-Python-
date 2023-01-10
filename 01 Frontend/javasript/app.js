09e009a90bc718c75505a0c70fc707bcff763c5c
mingyu <mingyu.oh15b@gmail.com>
2023-01-10 19:51:10 +0900

    javascript Event

--- 01 Frontend/javasript/app.js
+++ 01 Frontend/javasript/app.js
@@ -2,6 +2,46 @@ const queryID = document.querySelector("#qID")
 const queryClass = document.querySelector(".qClass")
 const queryAll = document.querySelectorAll(".qClass")
 
-console.log(queryID);
-console.log(queryClass);
-console.log(queryAll);
+
+function handleTitleClick() {
+    queryID.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    queryClass.style.color = "blue";
+}
+
+function handleMouseLeave() {
+    queryClass.innerText = "Mouse was visited"
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor="tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("No WIFI");
+}
+
+
+function handleWindowOnline() {
+    alert("ALL GOOD")
+}
+
+queryID.style.color = "red";
+queryID.addEventListener("click", handleTitleClick);
+queryClass.addEventListener("mouseenter", handleMouseEnter);
+queryClass.addEventListener("mouseleave", handleMouseLeave);
+
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
+
