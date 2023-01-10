a03897cbc02f188fdeab8ad560e419229fe65b59
mingyu <mingyu.oh15b@gmail.com>
2023-01-11 03:24:11 +0900

    javascript 로그인 기능-Local Storage

--- 01 Frontend/javasript/app.js
+++ 01 Frontend/javasript/app.js
@@ -1,19 +1,28 @@
-const LoginForm =  document.querySelector("#login-form");
-const LoginInput = document.querySelector("#login-form input");
+const loginForm =  document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("#login-form a");
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username";
 
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else {
+    greeting.innerText = `Hello ${saveUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 
 }
-
-LoginForm.addEventListener("submit", onLoginSubmit);
-
-link.addEventListener("click", handleLinkClick)
